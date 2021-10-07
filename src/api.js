4f11d21713984b1dd5cc4dd7a370b836d3962ee8
Lordkaito <cespedesisai88@gmail.com>
2021-10-07 14:43:15 +0200

    Aply api calls

---
+++ src/api.js
@@ -0,0 +1,54 @@
+const gameUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+const scoresUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/DVibYioPE4je7RGowm5U/scores';
+
+const newGame = async () => {
+  const newGameName = 'My new game';
+  const response = await fetch(gameUrl, {
+    method: 'POST',
+    body: JSON.stringify({ name: newGameName }),
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+  });
+  const jsonData = await response.json();
+  console.log(jsonData);
+  return jsonData;
+}
+
+const newUser = async (userValue, scoreValue) => {
+  const response = await fetch(scoresUrl, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: userValue,
+      score: scoreValue,
+    }),
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+  });
+  const jsonData = await response.json();
+  console.log(jsonData);
+  return jsonData;
+}
+
+const frameCreation = (arr, cont) => {
+  let players = '';
+  arr.forEach((player) => {
+    players += `<div id="scores">
+      <h3 class="score-name">${player.user}</h3>
+      <h3 class="score-points">${player.score}</h3>
+    </div>`;
+  });
+  cont.innerHTML = players;
+}
+
+const getScores = async (cont) => {
+  const response = await fetch(scoresUrl);
+  const jsonData = await response.json();
+  const sortArr = jsonData.result.sort((a, b) => b.score - a.score);
+  frameCreation(sortArr, cont);
+}
+
+// getScores(nameofmycontainer);
+
+export { newUser, getScores, newGame, frameCreation, gameUrl };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './style.css';
+import { newUser, getScores, newGame, frameCreation, gameUrl, scoresUrl } from './api.js';
+
+const userValue = document.querySelector('#name');
+const scoreValue = document.querySelector('#score');
+const points = document.querySelector('#points');
+const refresh = document.querySelector('#refresh');
+const submit = document.querySelector('#submit');
+const title = document.querySelector('#title');
+
+newGame();
+getScores(points);
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (userValue.value === '' || scoreValue.value === '') {
+    userValue.setCustomValidity('Please enter a name and score');
+    userValue.reportValidity();
+  } else if (scoreValue.value > 999) {
+    scoreValue.setCustomValidity('Score must be less than 999');
+    scoreValue.reportValidity();
+  } else {
+    newUser(userValue.value, scoreValue.value);
+    getScores(points);
+    userValue.value = '';
+    scoreValue.value = '';
+  }
+}
+);
+
+refresh.addEventListener('click', () => {
+  location.reload();
+}
+);
+
